9f938cd315d18b71c98bfbefabe5380e1213b733
Richard Chikeluba <chikelubarichard2m@gmail.com>
2026-01-08 21:06:55 -0500

    Added add-notes function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -8,9 +8,7 @@ CORS(app)
 def home():
     return "I am alive"
 
-@app.route("/get-notes")
-def get_notes():
-    return [
+notes = [
         {
             "title": "My first note",
             "body": "This is the body"
@@ -21,6 +19,36 @@ def get_notes():
         },
     ]
 
+@app.route("/get-notes")
+def get_notes():
+    return notes
+
+@app.route("/add-note", methods=["POST"])
+def add_note():
+    data = request.json
+
+    if ("title" not in data):
+        return jsonify(
+            {
+                "message": "You have no title"
+            }
+        ), 400
+
+    notes.append({
+        "title": data["title"],
+        "body": data["body"]
+    })
+ 
+    return [
+        {
+            "title": "My first note",
+            "body": "This is the body"
+        },
+        {
+            "title": "My second note",
+            "body": "This is the second body"
+        },
+    ]
 
 
 if __name__ == "__main__":
